f432fd89ad13143d177ea58e63658c0dc9b82eeb
ABDULRAHMAN ALASSAF <abodoassaf112@gmail.com>
2024-07-02 07:34:00 +0800

    edit-book-appointment-function

--- server/controlers/donor/bookAnAppointment.js
+++ server/controlers/donor/bookAnAppointment.js
@@ -1,11 +1,26 @@
 const db = require("../../dbinstance");
 
+function convertTo24Hour(timeSlot) {
+    const [time, modifier] = timeSlot.split(' ');
+    let [hours, minutes] = time.split(':');
+
+    if (hours === '12' && modifier === 'am') {
+        hours = '00'; // Correctly handle midnight
+    } else if (hours === '12' && modifier === 'pm') {
+        hours = '12'; // Correctly handle noon
+    } else if (modifier === 'pm') {
+        hours = parseInt(hours, 10) + 12; // Convert PM times to 24-hour format
+    }
+
+    return `${hours}:${minutes}`;
+}
+
 const bookAnAppointment = async function (req, res) {
     const { Donor_ID, DC_ID, Appointment_Date, Appointment_Time } = req.body;
     const query = "INSERT INTO `appointment` (Appointment_Date, Appointment_Time, Appointment_Status, Donor_ID, DC_ID) "
                 + "VALUES (?, ?, ?, ?, ?);";
 
-    const values = [Appointment_Date, Appointment_Time, "Pending", Donor_ID, DC_ID];
+    const values = [Appointment_Date, convertTo24Hour(Appointment_Time), "Pending", Donor_ID, DC_ID];
 
     try {
         const result = await db.promise().query(query, values);
