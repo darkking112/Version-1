3292d92327dbe1395ed891e9d6e08f87c12fea0f
ABDULRAHMAN ALASSAF <abodoassaf112@gmail.com>
2024-07-02 07:59:02 +0800

    update-removeDC-function

--- server/controlers/admin/removeDonationCenter.js
+++ server/controlers/admin/removeDonationCenter.js
@@ -5,14 +5,27 @@ const removeUser = async (id) => {
     await db.promise().query(query, [id]);
 }
 
+const getDonationCenterID = async (id) => {
+    const query = "SELECT DC_ID FROM `donation_center_list` where User_ID = ?";
+    const result = await db.promise().query(query, [id]);
+    return result[0][0];
+}
+
+const removeAdmin = async (id) => {
+    const query = "DELETE FROM `donation_center_admin` WHERE `User_ID` = ?";
+    await db.promise().query(query, [id]);
+}
+
 const removeDonationCenter = async (req, res) => {
     const { id } = req.params;
-    const query = "DELETE FROM `donation_center` WHERE `User_ID` = ?";
+    const {DC_ID} = await getDonationCenterID(id);
+    const query = "DELETE FROM `donation_center` WHERE `DC_ID` = ?";
 
     try {
-        const result = await db.promise().query(query, [id]);
+        const result = await db.promise().query(query, [DC_ID]);
         const { affectedRows } = result[0];
 
+        removeAdmin(id);
         if (affectedRows > 0) {
             await removeUser(id);
             return res.json("Donation Center removed successfully.");
